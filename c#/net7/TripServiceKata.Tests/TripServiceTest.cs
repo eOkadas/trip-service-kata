ed2505e3b03282b21c1f8687404ecf3d20ad9296
Etienne Delattre <ejm.delattre@gmail.com>
2023-03-20 09:23:07 +0000

    1st red test

---
+++ c#/net7/TripServiceKata.Tests/TripServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using TripServiceKata.Trip;
+using TripServiceKata.User;
+using TripServiceKata.Exception;
+
+namespace TripServiceKata.Tests;
+
+public class TripServiceTest
+{
+    [Fact]
+    public void Should_Throw_Exception_When_User_Not_LoggedIn()
+    {
+        ((Action)(() => new TripService().GetTripsByUser(new TripServiceKata.User.User())))
+        .Should()
+        .NotThrow();
+    }
+}
